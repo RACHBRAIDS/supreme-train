2530608d48ec318c5f5101fe757502b2ef5147d7
Cloud User <rachealagwe@gmail.com>
2024-12-21 15:13:26 +0000

    Modified

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 -Developers at work
+i can develop things
 -appliction is redy to be deployed to the development environment
 -development must be reviewed by managers before deployment
 - I adjusted this file 
